6f5ae88a5a271a41f2e4ca16a99b29012a036c90
Diego Navarro Tarraga <diego@macbook-pro-de-diego.local>
2016-03-14 18:52:59 +0100

    Initial Commit

---
+++ OpenLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenLibrary
+//
+//  Created by Diego Navarro Tarraga on 14/3/16.
+//  Copyright © 2016 Diego Navarro Tarraga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
